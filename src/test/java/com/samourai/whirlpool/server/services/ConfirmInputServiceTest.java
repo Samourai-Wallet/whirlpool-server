9ed2a8e3787c5a1420bc70dc0da9eb13d13dfad8
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2018-10-19 00:30:16 +0200

    add ConfirmInputServiceTest

---
+++ src/test/java/com/samourai/whirlpool/server/services/ConfirmInputServiceTest.java
@@ -0,0 +1,97 @@
+package com.samourai.whirlpool.server.services;
+
+import com.samourai.wallet.segwit.SegwitAddress;
+import com.samourai.whirlpool.protocol.websocket.notifications.MixStatus;
+import com.samourai.whirlpool.server.beans.Mix;
+import com.samourai.whirlpool.server.beans.TxOutPoint;
+import com.samourai.whirlpool.server.integration.AbstractIntegrationTest;
+import org.bitcoinj.core.ECKey;
+import org.bouncycastle.crypto.params.RSABlindingParameters;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.lang.invoke.MethodHandles;
+import java.math.BigInteger;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = DEFINED_PORT)
+public class ConfirmInputServiceTest extends AbstractIntegrationTest {
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Autowired
+    private RegisterInputService registerInputService;
+
+    @Autowired
+    private ConfirmInputService confirmInputService;
+
+    @Autowired
+    private RegisterOutputService registerOutputService;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    private void registerInput(Mix mix, String username) throws Exception {
+        String poolId = mix.getPool().getPoolId();
+
+        ECKey ecKey = ECKey.fromPrivate(new BigInteger("34069012401142361066035129995856280497224474312925604298733347744482107649210"));
+        byte[] pubkey = ecKey.getPubKey();
+        String signature = ecKey.signMessage(poolId);
+
+        long inputBalance = mix.getPool().computeInputBalanceMin(false);
+        TxOutPoint txOutPoint = rpcClientService.createAndMockTxOutPoint(new SegwitAddress(pubkey, cryptoService.getNetworkParameters()), inputBalance, 999);
+
+        registerInputService.registerInput(poolId, username, pubkey, signature, txOutPoint.getHash(), txOutPoint.getIndex(), false, true);
+    }
+
+    @Test
+    public void confirmInput_shouldSuccessWhenValid() throws Exception {
+        Mix mix = __getCurrentMix();
+        String mixId = mix.getMixId();
+        String username = "testusername";
+        String receiveAddress = "tb1qg3z38tda68guak84yltx5hp7lfwgjrczls2luu";
+
+
+        // REGISTER_INPUT
+        registerInput(mix, username);
+        testUtils.assertMix(0, 1, mix); // mustMix confirming
+
+
+        // blind bordereau
+        RSAKeyParameters serverPublicKey = (RSAKeyParameters) cryptoService.generateKeyPair().getPublic();
+        RSABlindingParameters blindingParams = clientCryptoService.computeBlindingParams(serverPublicKey);
+        byte[] blindedBordereau = clientCryptoService.blind(receiveAddress, blindingParams);
+
+        // CONFIRM_INPUT
+        confirmInputService.confirmInputOrQueuePool(mixId, username, blindedBordereau);
+
+
+        // get a valid signed blinded bordereau
+        byte[] signedBlindedBordereau = cryptoService.signBlindedOutput(blindedBordereau, mix.getKeyPair());
+
+
+        // REGISTER_OUTPUT
+        mix.setMixStatusAndTime(MixStatus.REGISTER_OUTPUT);
+        Assert.assertEquals(0, mix.getReceiveAddresses().size());
+        byte[] unblindedSignedBordereau = clientCryptoService.unblind(signedBlindedBordereau, blindingParams);
+        registerOutputService.registerOutput(mix.computeInputsHash(), unblindedSignedBordereau, receiveAddress);
+        Assert.assertEquals(1, mix.getReceiveAddresses().size());
+
+        // TEST
+
+        // VERIFY
+        testUtils.assertMix(1, 0, mix); // mustMix confirmed
+        Thread.sleep(5000);
+    }
+}
