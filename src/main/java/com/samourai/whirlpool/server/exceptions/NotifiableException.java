88e63cba7a4964ec7802c5effad3273358b48ca8
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2018-11-05 18:46:20 +0100

    cleanup exceptions

--- src/main/java/com/samourai/whirlpool/server/exceptions/NotifiableException.java
+++ src/main/java/com/samourai/whirlpool/server/exceptions/NotifiableException.java
@@ -1,13 +1,20 @@
 package com.samourai.whirlpool.server.exceptions;
 
+import java.lang.invoke.MethodHandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class NotifiableException extends Exception {
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
   public NotifiableException(String message) {
     super(message);
   }
 
   public static String computeNotifiableMessage(Exception e) {
-    String message =
-        NotifiableException.class.isAssignableFrom(e.getClass()) ? e.getMessage() : "Server error";
-    return message;
+    if (!NotifiableException.class.isAssignableFrom(e.getClass())) {
+      log.warn("Exception obfuscated to user", e);
+      return "Server error";
+    }
+    return e.getMessage();
   }
 }
