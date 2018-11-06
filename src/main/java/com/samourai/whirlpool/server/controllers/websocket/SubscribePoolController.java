d3c95148d60aea9a0d891b460b8e393f75b77e95
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2018-11-06 14:42:04 +0100

    delay susbcription response

--- src/main/java/com/samourai/whirlpool/server/controllers/websocket/SubscribePoolController.java
+++ src/main/java/com/samourai/whirlpool/server/controllers/websocket/SubscribePoolController.java
@@ -2,7 +2,6 @@ package com.samourai.whirlpool.server.controllers.websocket;
 
 import com.samourai.whirlpool.protocol.WhirlpoolProtocol;
 import com.samourai.whirlpool.protocol.websocket.messages.SubscribePoolResponse;
-import com.samourai.whirlpool.server.services.CryptoService;
 import com.samourai.whirlpool.server.services.PoolService;
 import com.samourai.whirlpool.server.services.WebSocketService;
 import java.lang.invoke.MethodHandles;
@@ -10,6 +9,7 @@ import java.security.Principal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
@@ -18,16 +18,17 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class SubscribePoolController extends AbstractWebSocketController {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private static final int SUBSCRIBE_RESPONSE_DELAY = 1000;
 
   private PoolService poolService;
-  private CryptoService cryptoService;
+  private TaskExecutor taskExecutor;
 
   @Autowired
   public SubscribePoolController(
-      PoolService poolService, WebSocketService webSocketService, CryptoService cryptoService) {
+      PoolService poolService, WebSocketService webSocketService, TaskExecutor taskExecutor) {
     super(webSocketService);
     this.poolService = poolService;
-    this.cryptoService = cryptoService;
+    this.taskExecutor = taskExecutor;
   }
 
   @SubscribeMapping(
@@ -45,7 +46,19 @@ public class SubscribePoolController extends AbstractWebSocketController {
     String headerPoolId = getHeaderPoolId(headers);
     SubscribePoolResponse subscribePoolResponse =
         poolService.computeSubscribePoolResponse(headerPoolId);
-    getWebSocketService().sendPrivate(username, subscribePoolResponse);
+
+    // delay to make sure client processed subscription before sending him private response
+    taskExecutor.execute(
+        () -> {
+          synchronized (this) {
+            try {
+              wait(SUBSCRIBE_RESPONSE_DELAY);
+            } catch (InterruptedException e) {
+            }
+          }
+          // send reply
+          getWebSocketService().sendPrivate(username, subscribePoolResponse);
+        });
   }
 
   private String getHeaderPoolId(StompHeaderAccessor headers) {
